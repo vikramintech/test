c90bbe93615f184c412d2d4511cb2970af39b770
Vikram Singh Rana <116163333+vikramsinghranain@users.noreply.github.com>
2022-10-31 15:58:08 +0530

    Add files via upload

---
+++ 01-variables.js
@@ -0,0 +1,7 @@
+let hasAccount = false;
+let hasJob = false;
+let hasCredit = false;
+let userAge = prompt("Enter the age of the customer:");// input given by the user
+if(userAge>=18||userAge>=15){
+    
+}
